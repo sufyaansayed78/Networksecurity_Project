a2d1070c1fe315b50bee4f5881ee861711f34d1d
Sufyaan Sayed <sufyaansayed78@gmail.com>
2025-09-28 15:27:13 +0100

    ML Flow implemented

--- Networksecurity/Components/Model_trainer.py
+++ Networksecurity/Components/Model_trainer.py
@@ -19,7 +19,7 @@ from sklearn.ensemble import(
 
 from Networksecurity.utils.main_utils.utils1 import evaluate_models
 from Networksecurity.utils.main_utils.utils1 import NetworkModel
-
+import mlflow
 
 
 
@@ -31,6 +31,21 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e:
             raise NetwrorkSecurityException(e,sys)
+        
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+            
+
+
+
         
     def train_model(self,x_train,y_train,x_test,y_test):
         models = {
@@ -75,6 +90,9 @@ class ModelTrainer:
         best_model = models[best_model_name]
         y_train_pred = best_model.predict(x_train)
         classification_train_metric = classification_matrix(y_true=y_train,y_pred=y_train_pred)
+        self.track_mlflow(best_model,classification_train_metric)
+
+
         y_test_pred = best_model.predict(x_test)
         classification_test_metric = classification_matrix(y_true=y_test,y_pred=y_test_pred)
         preprocessor = load_object(filepath=self.data_transformation_artifact.transformed_object_file_path)
